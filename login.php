0579589d0b3f3c2ecc2ff2a8d24921995c91a2af
CraftiiX <le.locat.leo@gmail.com>
2024-06-14 10:57:37 +0200

    button css login

--- login.php
+++ login.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion a Confiz</title>
     <link rel="stylesheet" href="login.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Hind+Vadodara:wght@300;400;500;600;700&display=swap');
+    </style>
 </head>
 
 <?php
